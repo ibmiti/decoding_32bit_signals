d728b6eefdda613775bb04e309262cafbb2359d1
Dimitri M <ibmiti@gmail.com>
2020-12-04 08:02:31 -0800

    putting together the 10 possible signals

---
+++ radio_signal.php
@@ -0,0 +1,49 @@
+<?php 
+
+// radio signal has these predescribed codes : 
+// these codes will come from the radio as 32 bit numbers 
+$possible_decoded_output_radio_signals = [
+10,
+12,
+10,
+9,
+1,
+1,
+1,
+1,
+8,
+2,
+3
+];
+
+// representation of each in its 32 bit format :
+
+$possible_decoded_output_radio_signals[0];
+$possible_decoded_output_radio_signals[1];
+$possible_decoded_output_radio_signals[2];
+$possible_decoded_output_radio_signals[3];
+$possible_decoded_output_radio_signals[4];
+$possible_decoded_output_radio_signals[5];
+$possible_decoded_output_radio_signals[6];
+$possible_decoded_output_radio_signals[7];
+$possible_decoded_output_radio_signals[8];
+$possible_decoded_output_radio_signals[9];
+$possible_decoded_output_radio_signals[10];
+
+
+// shorten arr name as it will be used again , avoid brevity 
+
+function encode_
+for ($i = 0; $i <= count($raw32) - 1; $i++){
+
+}
+
+
+
+
+function radio_signal_input(array $output_radio_signals){
+	s1 = output_radio_signals[0];
+
+}
+$b = sprintf("%032b",decbin($output_radio_signals[0]));
+// I will need a system that calls rules from json file and 
