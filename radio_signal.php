ad7cb05d4e8c30abc61cbc8f722bf70d186d9d2d
Dimitri M <ibmiti@gmail.com>
2020-12-04 11:52:04 -0800

    finished signal translater

--- radio_signal.php
+++ radio_signal.php
@@ -1,21 +1,5 @@
 <?php 
 
-// radio signal has these predescribed codes : 
-// these codes will come from the radio as 32 bit numbers 
-$possible_decoded_output_radio_signals = [
-10,
-12,
-10,
-9,
-1,
-1,
-1,
-1,
-8,
-2,
-3
-];
-
 // test case function that outputs signals represented as integers ( 1 - 15 )
 function radio_transmissions(){
 	$signals = array();
@@ -32,29 +16,15 @@ function radio_transmissions(){
 }
 
 $signals = radio_transmissions();
-print_r($signals);
+// print_r($signals);
 
 
 // function to accept 32 bit numbers - and reduce them down to integers
-function signal_decoder($signal){
-	$signal_binary =  bindec($signal);
-	echo bindec($signal_binary);
-}
-
-
-// shorten arr name as it will be used again , avoid brevity 
-
-function encode_
-for ($i = 0; $i <= count($raw32) - 1; $i++){
+function signal_decoder($signals){
+	foreach($signals as $signal){
+		$signal_binary =  bindec($signal);
+		$signal_full_translated = bindec($signal_binary);
+	}
 
 }
 
-
-
-
-function radio_signal_input(array $output_radio_signals){
-	s1 = output_radio_signals[0];
-
-}
-$b = sprintf("%032b",decbin($output_radio_signals[0]));
-// I will need a system that calls rules from json file and 
